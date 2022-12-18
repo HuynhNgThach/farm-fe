1fb8750cd40ba511606a2cb74cdfbd6597c3ee0f
Thach Huynh <ngocthach@ngocthach.local>
2022-12-18 08:10:51 +0700

    add menu

--- src/components/layouts/AppDrawer.vue
+++ src/components/layouts/AppDrawer.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-navigation-drawer elevation="5" width="256">
+  <v-navigation-drawer elevation="5" width="300">
     <v-list>
       <v-list-item
         prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
@@ -10,17 +10,32 @@
 
     <v-divider></v-divider>
 
-    <v-list density="compact" nav>
+    <v-list density="compact" nav class="menu">
+      <v-list-subheader>Apps & tool</v-list-subheader>
+
       <v-list-item
         prepend-icon="mdi-folder"
         title="My Files"
         value="myfiles"
       ></v-list-item>
-      <v-list-item
-        prepend-icon="mdi-account-multiple"
-        title="Encode Tool"
-        value="Encode Tool"
-      ></v-list-item>
+      <v-list-group value="Encode/decode tools">
+        <template v-slot:activator="{ props }">
+          <v-list-item
+            prepend-icon="mdi-star"
+            v-bind="props"
+            title="Encode/decode tools"
+          ></v-list-item>
+        </template>
+
+        <v-list-item
+          :title="'Base64 encode/decode'"
+          :value="'Base64 encode/decode'"
+        ></v-list-item>
+        <v-list-item
+          :title="'Base64 encode/decode'"
+          :value="'Base64 encode/decode1'"
+        ></v-list-item>
+      </v-list-group>
       <v-list-item
         prepend-icon="mdi-star"
         title="Encode Tool"
@@ -50,8 +65,12 @@ export default {
 
 <style lang="scss">
 .v-navigation-drawer {
+  border-right: unset;
   .v-navigation-drawer__content {
     background-color: rgb(var(--v-theme-blockColor)) !important;
+  }
+  .menu {
+    margin-top: 30px;
   }
 }
 </style>
